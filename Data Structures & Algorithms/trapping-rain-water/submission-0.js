9df24a39e02f083bb701ae0f1d51514d1b4120b3
Kashif Sheikh <56736628+kashif9598@users.noreply.github.com>
2026-06-12 00:00:28 +0530

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.js
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} height
+     * @return {number}
+     */
+    trap(height) {
+        let n = height.length;
+        let maxL=[];
+        maxL[0] = height[0];
+        for(let i=1;i<n;i++){
+            maxL[i] = Math.max(maxL[i-1], height[i])
+        }
+
+        let maxR = [];
+        maxR[n-1] = height[n-1];
+        for(let i=n-2;i>=0;i--){
+            maxR[i] = Math.max(maxR[i+1], height[i])
+        }
+
+        let ans = 0;
+        for(let i=0;i<n;i++){
+            let water = Math.min(maxL[i], maxR[i]) - height[i];
+            ans += Math.max(water, 0)
+        }
+
+        return ans;
+    }
+}
