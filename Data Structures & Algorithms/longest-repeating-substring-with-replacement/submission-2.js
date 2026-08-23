37de308629ec2007abfe7829e59398ccabe92e14
Kashif Sheikh <56736628+kashif9598@users.noreply.github.com>
2026-08-23 19:48:52 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let longest = 0;
+        const freq = {};
+        let maxFreq = 0;
+        let left = 0;
+        for (let right = 0; right < s.length; right++) {
+            freq[s[right]] = (freq[s[right]] || 0) + 1;
+
+            maxFreq = Math.max(maxFreq, freq[s[right]]);
+
+            while (right - left + 1 - maxFreq > k) {
+                freq[s[left]]--;
+                left++;
+            }
+            longest = Math.max(longest, right - left + 1);
+        }
+        return longest;
+    }
+}
