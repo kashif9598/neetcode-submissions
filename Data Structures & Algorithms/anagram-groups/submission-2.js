ff0768987d52e5dae0bf32d34981d4d6af4e5e5c
Kashif Sheikh <56736628+kashif9598@users.noreply.github.com>
2026-09-01 11:40:02 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let map = {};
+        for(let i=0;i<strs.length;i++){
+            const sortedStr = strs[i].split('').sort().join('');
+            if(map[sortedStr]){
+                map[sortedStr].push(strs[i])
+            } else {
+                map[sortedStr] = [strs[i]];
+            }
+        }
+        return Object.values(map)
+    }
+}
